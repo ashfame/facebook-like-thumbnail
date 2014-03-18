48f988383d39a46079bc95e613bdb615e1003085
Ashfame <mail@ashfame.com>
2014-03-19 03:55:01 +0530

    complete rewrite for v0.3

--- admin.php
+++ admin.php
@@ -2,117 +2,102 @@
 /**
  * Function to create an options page
  */
-add_action( 'admin_menu', 'fb_like_thumbnail_page' );
 
-function fb_like_thumbnail_page() {
-	$fb_like_thumbnail_admin_hook = add_options_page( 'Facebook Like Thumbnail Setting', 'Facebook Like Thumbnail', 'manage_options', 'fb-like-thumbnail', 'fb_like_thumbnail_options_page' );
-	
-	// add CSS styles specific to our options page on our options page only
-	add_action( "admin_head-{$fb_like_thumbnail_admin_hook}", 'fb_like_thumbnail_admin_style' );	
-}
+class Ashfame_Facebook_Like_Thumbnail_Admin {
 
-/**
- * Function to add CSS styles on our Options page
- */
-function fb_like_thumbnail_admin_style() {
-?>
-	<style type="text/css">
-		.wrap ul {
-			list-style-type:disc;
-			margin:10px 0 10px 15px;
-		}
-		.wrap ul ul {
-			list-style-type:circle;
-		}
-	</style>
-<?php
-}
+	public function __construct() {
+		// Add options page
+		add_action( 'admin_menu', array( $this, 'fb_like_thumbnail_page' ) );
+		// Settings API
+		add_action( 'admin_init', array( $this, 'fb_like_thumbnail_init' ) );
+	}
 
-/**
- * Function to draw the options page
- */
-function fb_like_thumbnail_options_page() {
-?>
-	<div class="wrap">
-		<?php screen_icon( 'plugins' ); ?>
-		<h2>Facebook Like Thumbnail Setting</h2>
-		
-		<form action="options.php" method="post">
-			<?php settings_fields( 'fb_like_thumbnail_options' ); ?>
-			<?php do_settings_sections( 'fb_like_thumbnail' ); ?>
-			<br />
-			<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e( 'Save' ); ?>" />
-		</form>
-	</div>
-<?php	
-}
+	public function fb_like_thumbnail_page() {
+		$fb_like_thumbnail_admin_hook = add_options_page( 'Facebook Like Thumbnail Setting', 'Facebook Like Thumbnail', 'manage_options', 'fb-like-thumbnail', array( $this, 'fb_like_thumbnail_options_page' ) );
 
-/**
- * WordPress Settings API to save plugin's data
- */
-add_action( 'admin_init', 'fb_like_thumbnail_init' );
+		// add CSS styles specific to our options page on our options page only
+		add_action( "admin_head-{$fb_like_thumbnail_admin_hook}", array( $this, 'fb_like_thumbnail_admin_style' ) );
+	}
 
-function fb_like_thumbnail_init() {
-	register_setting(
-		'fb_like_thumbnail_options',	// same to the settings_field
-		'fb_like_thumbnail',		// options name
-		'fb_like_thumbnail_validate'	// validation callback
-	);
-	add_settings_section(
-		'default',			// settings section (a setting page must have a default section, since we created a new settings page, we need to create a default section too)
-		'Main settings',		// section title
-		'fb_like_section_text',		// text for the section
-		'fb_like_thumbnail'		// specify the output of this section on the options page, same as in do_settings_section
-	);
-	add_settings_field(
-		'fb_like_default',		// field ID
-		'Default FB Like thumbnail',	// Field title
-		'fb_like_default_setting',	// display callback
-		'fb_like_thumbnail',		// which settings page?
-		'default'			// which settings section?
-	);
-}
+	/**
+	 * Function to add CSS styles on our Options page
+	 */
+	public function fb_like_thumbnail_admin_style() {
+		?>
+		<style type="text/css">
+			.wrap ul {
+				list-style-type:disc;
+				margin:10px 0 10px 15px;
+			}
+			.wrap ul ul {
+				list-style-type:circle;
+			}
+		</style>
+	<?php
+	}
 
-function fb_like_section_text() {
-	echo '<p><img src="'.plugins_url( 'example.png', __FILE__ ).'" alt="FB like Example" title="Example" /></p>
-	The plugin picks the thumbnail in the following order (whichever is found first)
-	<ul>
-		<li>If its a post or page
-			<ul>
-				<li>Featured Thumbnail</li>
-				<li>First Image in the post</li>
-				<li>Default</li>
-			</ul>
-		</li>
-		<li>If its a Front Page or Search Page
-			<ul>
-				<li>Default</li>
-			</ul>
-		</li>
-		<li>Anything else
-			<ul>
-				<li>First image in the first post of the loop</li>
-				<li>Default</li>
-			</ul>
-		</li>
-	</ul>
-	</p>';
-}
+	/**
+	 * Function to draw the options page
+	 */
+	public function fb_like_thumbnail_options_page() {
+		?>
+		<div class="wrap">
+			<h2>Facebook Like Thumbnail</h2>
 
-function fb_like_default_setting() {
-	$options = get_option( 'fb_like_thumbnail' );
-	echo "<input id='fb_like_default' name='fb_like_thumbnail[default]' size='60' type='text' value='{$options['default']}' />";
-}
+			<form action="options.php" method="post">
+				<?php settings_fields( 'fb_like_thumbnail_options' ); ?>
+				<?php do_settings_sections( 'fb_like_thumbnail' ); ?>
+				<br />
+				<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e( 'Save' ); ?>" />
+			</form>
+		</div>
+	<?php
+	}
 
-function fb_like_thumbnail_validate( $input ) {
-	$valid['default'] = esc_url_raw( $input['default'] );
-	if ( $valid['default'] != $input['default'] ) {
-		add_settings_error(
-			'fb_like_default',				// title (?)
-			'fb_like_default_url_error',			// error ID (?)
-			'Invalid link! Please enter a proper link',	// error message
-			'error'						// message type
+	public function fb_like_thumbnail_init() {
+		register_setting(
+			'fb_like_thumbnail_options',	// same to the settings_field
+			'fb_like_thumbnail',		// options name
+			array( $this, 'fb_like_thumbnail_validate' )	// validation callback
+		);
+		add_settings_section(
+			'default',			// settings section (a setting page must have a default section, since we created a new settings page, we need to create a default section too)
+			'Main settings',		// section title
+			array( $this, 'fb_like_section_text' ),		// text for the section
+			'fb_like_thumbnail'		// specify the output of this section on the options page, same as in do_settings_section
+		);
+		add_settings_field(
+			'fb_like_default',		// field ID
+			'Default FB Like thumbnail',	// Field title
+			array( $this, 'fb_like_default_setting' ),	// display callback
+			'fb_like_thumbnail',		// which settings page?
+			'default'			// which settings section?
 		);
 	}
-	return $valid;	
+
+	public function fb_like_section_text() {
+		?>
+		Specify the default fallback image to be used. Using a logo or something similar is recommened.
+		<?php
+	}
+
+	function fb_like_default_setting() {
+		$options = get_option( 'fb_like_thumbnail' );
+		echo "<input id='fb_like_default' name='fb_like_thumbnail[default]' size='60' type='text' value='{$options['default']}' />";
+	}
+
+	function fb_like_thumbnail_validate( $input ) {
+		$valid['default'] = esc_url_raw( $input['default'] );
+		if ( $valid['default'] != $input['default'] ) {
+			add_settings_error(
+				'fb_like_default',				// title (?)
+				'fb_like_default_url_error',			// error ID (?)
+				'Invalid link! Please enter a proper link',	// error message
+				'error'						// message type
+			);
+		}
+		return $valid;
+	}
 }
+
+new Ashfame_Facebook_Like_Thumbnail_Admin();
--- facebook-like-thumbnail.php
+++ facebook-like-thumbnail.php
@@ -1,57 +1,121 @@
 <?php
-/*
-Plugin Name: Facebook Like Thumbnail
-Plugin URI: http://blog.ashfame.com/?p=888
-Description: Sets the first image of the post as the thumbnail which shows up on FB like and share
-Author: Ashfame
-Author URI: http://blog.ashfame.com/
-Version: 0.2
-License: GPL
+/**
+ * Plugin Name: Facebook Like Thumbnail
+ * Plugin URI: http://blog.ashfame.com/?p=888
+ * Description: Figures out the thumbnail image to be used for Facebook & Other sites depending on meta:og tag
+ * Author: Ashfame
+ * Author URI: http://blog.ashfame.com/
+ * Version: 0.3
+ * License: GPL
+ * Notes: This plugin only do the job of assigning meta:og image to pages. Title and description are easily picked up from meta info or derived from page's content.
+ */
 
-@todo Add author avatar for author pages
-@todo Ignore smilies as a first match
-*/
+// die if called directly
+defined( 'ABSPATH' ) || die();
 
 // If we are at WordPress Admin side, load the file for option page
-if ( is_admin() )
-	require plugin_dir_path( __FILE__ ).'admin.php';
+is_admin() && require plugin_dir_path( __FILE__ ).'admin.php';
 
-add_action( 'wp_head', 'fb_like_thumbnails' );
+class Ashfame_Facebook_Like_Thumbnail {
 
-function fb_like_thumbnails()
-{
-	global $posts;
-	$options = get_option('fb_like_thumbnail');	
-	$default = $options['default'];
-	
-	if ( is_front_page() || is_search() ) {
-		$thumb = $default;
-	} else {
-		$thumb_set = false;
-		
-		if ( is_single() || is_page() ) {
-			if ( function_exists( 'has_post_thumbnail' ) ) { // compatibility with themes who doesn't support featured thumbnails
-				if ( has_post_thumbnail( $posts[0]->ID ) ) {
-					$thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $posts[0]->ID) );
-					$thumb = $thumb[0]; // take the URL from the array
-					$thumb_set = true;
+	public static $version = '0.3';
+	public static $options;
+	public static $meta_og_image; // Static variable so that it can be reused for custom code, for instance when constructing pinterest sharing link, this media image can be used
+	public static $meta_og_where = 'default fallback';
+
+	public function __construct() {
+		// set defaults
+		self::$options = get_option( 'fb_like_thumbnail' );
+		self::$meta_og_image = apply_filters( 'fb_like_thumbnail_default', self::$options['default'] );
+
+		// This figures out the image to use once so that wherever its needed, its just retrieved from here, saving multiple code which makes same sort of SQL queries again
+		add_action( 'wp', array( $this, 'figure_out_media_image_for_social_networks' ) );
+		add_action( 'wp_head', array( $this, 'output_meta_og_image_tag' ) );
+
+		// Paid Support link in plugins listing
+		add_filter( 'plugin_action_links', array( $this, 'support_plugin_action_link' ), 10, 2 );
+	}
+
+	public function bail() {
+		// WooCommerce pages
+		if ( is_page( 'cart' ) || is_page( 'checkout' ) || is_page( 'my-account' ) ) {
+			return true;
+		}
+
+		return apply_filters( 'fb_like_thumbnail_bail', false ); // Filter here if you need to set exclusion on pages, best to do this on pages which are only meant for logged in users, saves database queries
+	}
+
+	public function figure_out_media_image_for_social_networks() {
+		$media = '';
+
+		// bail
+		if ( $this->bail() ) {
+			return;
+		}
+
+		// short-circuit opportunity
+		$media = apply_filters( 'fb_like_thumbnail_shortcircuit', '' ); // Short-circuit here if you want to change the logic of image selection here
+		if ( ! empty( $media ) ) {
+			self::$meta_og_where = 'shortcircuit';
+		}
+
+		// check for image inside posts, if we are on a archive page, only if needed
+		if ( empty( $media ) && is_archive() ) {
+			global $posts;
+
+			foreach ( $posts as $post ) {
+				$media = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
+				if ( $media ) {
+					self::$meta_og_where = 'image from posts loop - archive page';
+					break;
 				}
 			}
 		}
-		
-		if ( !$thumb_set ) {
-			$content = do_shortcode( $posts[0]->post_content ); // $posts is an array, fetch the first element
-			$output = preg_match_all( '/<img[^>]+src=[\'"]([^\'"]+)[\'"].*>/i', $content, $matches);
-			if ( $output > 0 ) {
-				$thumb = $matches[1][0];
-				// #dirtyhack - check for nextgen loader file (if embedded as slideshow), and fallback to default
-				if ( strpos( $thumb, 'nextgen-gallery/images/loader.gif' ) ) {
-					$thumb = $default;
-				}
-			} else
-				$thumb = $default;
+
+		// check for featured thumbnail, if needed
+		if ( empty( $media ) && get_post_thumbnail_id() ) {
+			$media = wp_get_attachment_url( get_post_thumbnail_id() );
+			self::$meta_og_where = 'featured thumbnail';
+		}
+
+		// check for attachments (ACF modules or any sort of gallery plugins should be covered here), only if needed
+		if ( empty( $media ) && is_singular() ) {
+
+			$attachments = get_posts(
+				array(
+					'post_type' => 'attachment',
+					'numberposts' => 1,
+					'post_status' => 'publish',
+					'post_parent' => get_the_ID()
+				)
+			);
+
+			if ( $attachments ) {
+				$media = wp_get_attachment_thumb_url( $attachments[0]->ID ); // @TODO possible filtering of attachments by extensions here, not all attachments are going to be media files
+				self::$meta_og_where = 'first attachment';
+			}
+		}
+
+		// assign media image for using it if we managed to get a specific to context, else default will be used
+		if ( ! empty( $media ) ) {
+			self::$meta_og_image = $media;
 		}
 	}
 
-	echo "\n\n<!-- Facebook Like Thumbnail -->\n<meta property=\"og:image\" content=\"$thumb\" />\n<!-- End Facebook Like Thumbnail -->\n\n";
+	public function output_meta_og_image_tag() {
+		echo "\n\n<!-- Facebook Like Thumbnail (v" . self::$version . ") -->\n" . '<meta property="og:image" content="' . self::$meta_og_image . '" />' . "\n<!-- using " . self::$meta_og_where . " -->\n<!-- Facebook Like Thumbnail (By Ashfame - https://github.com/ashfame/facebook-like-thumbnail) -->\n\n";
+	}
+
+	public function support_plugin_action_link( $links, $file ) {
+		// Also check using strpos because when plugin is actually a symlink inside plugins folder, its plugin_basename will be based off its actual path
+		if ( $file == plugin_basename( __FILE__ ) || strpos( plugin_basename( __FILE__ ), $file ) !== false ) {
+			$settings_link = '<a href="' . admin_url( 'options-general.php?page=fb-like-thumbnail' ) . '">Settings</a>';
+			$support_link = '<a href="mailto:mail@ashfame.com">Premium Support</a>';
+			$links = array_merge( array( $settings_link, $support_link ), $links );
+		}
+
+		return $links;
+	}
 }
+
+new Ashfame_Facebook_Like_Thumbnail();
